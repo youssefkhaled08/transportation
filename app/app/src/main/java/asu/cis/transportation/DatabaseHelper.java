20f22743fc728a96533c284d6db561024ea12bc1
youssef ashraf <youssefetoo780@gmail.com>
2021-08-17 23:03:12 +0200

    Creating Tables

--- app/app/src/main/java/asu/cis/transportation/DatabaseHelper.java
+++ app/app/src/main/java/asu/cis/transportation/DatabaseHelper.java
@@ -16,11 +16,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String TRAVELS_TABLE = "TRAVELS_TABLE";
     public static final String TRAVELS_COLUMN_ID = "ID";
-    public static final String TRAVELS_COLUMN_FROM = "FROM";
-    public static final String TRAVELS_COLUMN_TO = "TO";
+    public static final String TRAVELS_COLUMN_FROM = "BEGINING";
+    public static final String TRAVELS_COLUMN_TO = "DESTINATION";
     public static final String TRAVELS_COLUMN_DATE = "DATE";
-    public static final String TRAVELS_COLUMN_START = "START";
-    public static final String TRAVELS_COLUMN_END = "END";
+    public static final String TRAVELS_COLUMN_START = "START_TIME";
+    public static final String TRAVELS_COLUMN_END = "END_TIME";
     public static final String TRAVELS_COLUMN_PRICE = "PRICE";
     public static final String TRAVELS_COLUMN_AVAILABLE_SEATS = "AVAILABLE_SEATS";
 
@@ -32,8 +32,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String createUserTable = "CREATE TABLE " + USER_TABLE + " (" + USER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + USER_COLUMN_NAME + " TEXT, " + USER_COLUMN_EMAIL + " TEXT, " + USER_COLUMN_PASSWORD + " TEXT)";
         String createTravelsTable = "CREATE TABLE " + TRAVELS_TABLE + " (" + TRAVELS_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TRAVELS_COLUMN_FROM + " TEXT, " + TRAVELS_COLUMN_TO + " TEXT, " + TRAVELS_COLUMN_DATE + " TEXT, " + TRAVELS_COLUMN_START + " TEXT, " + TRAVELS_COLUMN_END + " TEXT, " + TRAVELS_COLUMN_PRICE + " REAL, " + TRAVELS_COLUMN_AVAILABLE_SEATS + " INTEGER)";
-
+        db.execSQL(createUserTable);
         db.execSQL(createTravelsTable);
+
     }
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
