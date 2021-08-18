f4f55e030f7c371d4fd43520555bb25c9fd39cac
Youssef Khaled <youssefkfarouk@gmail.com>
2021-08-19 01:29:14 +0200

    Added splash screen

--- app/app/src/main/java/asu/cis/transportation/MainActivity.java
+++ app/app/src/main/java/asu/cis/transportation/MainActivity.java
@@ -1,14 +1,34 @@
 package asu.cis.transportation;
 
+import static android.net.sip.SipErrorCode.TIME_OUT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static int TIME_OUT = 3000; //Time to launch the another activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        final View myLayout = findViewById(R.id.splashScreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
