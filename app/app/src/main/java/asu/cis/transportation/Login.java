b9ebb50070b8c4b859c6423308bd1ffe82bdd803
Youssef Khaled <youssefkfarouk@gmail.com>
2021-08-19 18:46:07 +0200

    Added and linked register page

--- app/app/src/main/java/asu/cis/transportation/Login.java
+++ app/app/src/main/java/asu/cis/transportation/Login.java
@@ -2,13 +2,27 @@ package asu.cis.transportation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRegister = findViewById(R.id.btnToRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
